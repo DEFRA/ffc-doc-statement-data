5d4a49b49ebb3b9acb0862ec24ba1619d535608e
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-03-06 11:36:22 +0000

    add mocks

---
+++ test/mocks/dax.js
@@ -0,0 +1,29 @@
+const mockDax1 = {
+  paymentReference: 'IN0056999',
+  calculationId: 123759086,
+  paymentPeriod: '1st May 2024 to 31st July 2024',
+  totalQuarterlyPayment: -3495,
+  transDate: new Date(2022, 7, 5, 15, 30, 10, 120)
+}
+
+const mockDax2 = {
+  paymentReference: 'IN0056990',
+  calculationId: 123759087,
+  paymentPeriod: '1st May 2024 to 31st July 2024',
+  totalQuarterlyPayment: -4495,
+  transDate: new Date(2022, 7, 5, 15, 30, 10, 120)
+}
+
+const mockDax3 = {
+  paymentReference: 'IN0056991',
+  calculationId: 123759088,
+  paymentPeriod: '1st May 2024 to 31st July 2024',
+  totalQuarterlyPayment: -5495,
+  transDate: new Date(2022, 7, 5, 15, 30, 10, 120)
+}
+
+module.exports = {
+  mockDax1,
+  mockDax2,
+  mockDax3
+}
---
+++ test/mocks/totals.js
@@ -0,0 +1,53 @@
+const mockTotal1 = {
+  calculationId: 1234567,
+  sbi: 123456789,
+  frn: 1234567890,
+  agreementNumber: 6660999,
+  claimId: 9999999,
+  schemeType: 'SFI Actions (SFIMT)',
+  calculationDate: new Date(2024, 7, 5, 15, 30, 10, 120),
+  invoiceNumber: 'SFIA1234567',
+  agreementStart: new Date(2023, 7, 5, 15, 30, 10, 120),
+  agreementEnd: new Date(2026, 7, 5, 15, 30, 10, 120),
+  totalAdditionalPayments: 1000,
+  totalActionPayments: 13455.99,
+  updated: new Date(2024, 7, 5, 15, 30, 10, 120)
+}
+
+const mockTotal2 = {
+  calculationId: 1234567,
+  sbi: 123456789,
+  frn: 1234567890,
+  agreementNumber: 6660999,
+  claimId: 9999999,
+  schemeType: 'SFI Actions (SFIMT)',
+  calculationDate: new Date(2024, 7, 5, 15, 30, 10, 120),
+  invoiceNumber: 'SFIA1234567',
+  agreementStart: new Date(2023, 7, 5, 15, 30, 10, 120),
+  agreementEnd: new Date(2026, 7, 5, 15, 30, 10, 120),
+  totalAdditionalPayments: 1000,
+  totalActionPayments: 13455.99,
+  updated: new Date(2024, 7, 5, 15, 30, 10, 120)
+}
+
+const mockTotal3 = {
+  calculationId: 1234567,
+  sbi: 123456789,
+  frn: 1234567890,
+  agreementNumber: 6660999,
+  claimId: 9999999,
+  schemeType: 'SFI Actions (SFIMT)',
+  calculationDate: new Date(2024, 7, 5, 15, 30, 10, 120),
+  invoiceNumber: 'SFIA1234567',
+  agreementStart: new Date(2023, 7, 5, 15, 30, 10, 120),
+  agreementEnd: new Date(2026, 7, 5, 15, 30, 10, 120),
+  totalAdditionalPayments: 1000,
+  totalActionPayments: 13455.99,
+  updated: new Date(2024, 7, 5, 15, 30, 10, 120)
+}
+
+module.exports = {
+  mockTotal1,
+  mockTotal2,
+  mockTotal3
+}
