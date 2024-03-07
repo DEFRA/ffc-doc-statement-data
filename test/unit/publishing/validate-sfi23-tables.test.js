da15c5ad9eaf191396cfa2e19a16eb0c06fb1a14
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-03-07 10:19:35 +0000

    added dax tests

--- test/unit/publishing/validate-sfi23-tables.test.js
+++ test/unit/publishing/validate-sfi23-tables.test.js
@@ -1,23 +1,14 @@
 const { DAX, TOTALS } = require('../../../app/publishing/types')
 const validateUpdate = require('../../../app/publishing/validate-update')
 const { mockDax1 } = require('../../mocks/dax')
-const { mockAction1 } = require('../../mocks/actions')
 const { mockTotal1 } = require('../../mocks/totals')
 let dax
-let action
 let total
 
-describe('validate SFI-23 New Tables', () => {
+describe('validate SFI-23 TOTALS', () => {
   beforeEach(() => {
-    dax = JSON.parse(JSON.stringify(mockDax1))
-    dax.type = DAX
     total = JSON.parse(JSON.stringify(mockTotal1))
-    total.totalReference = total.calculationId
     total.type = TOTALS
-    action = JSON.parse(JSON.stringify(mockAction1))
-    delete total.calculationId
-    delete dax.paymentReference
-    delete action.actionCode
   })
 
   test('returns false if total does not contain sbi', () => {
@@ -57,6 +48,58 @@ describe('validate SFI-23 New Tables', () => {
   })
   test('returns falsy if total does not contain invoice number', () => {
     delete total.invoiceNumber
+    const result = validateUpdate(total, TOTALS)
+    expect(result).toBeFalsy()
+  })
+  test('returns falsy if total does not contain agreement start', () => {
+    delete total.agreementStart
+    const result = validateUpdate(total, TOTALS)
+    expect(result).toBeFalsy()
+  })
+  test('returns falsy if total does not contain agreement end', () => {
+    delete total.agreementEnd
+    const result = validateUpdate(total, TOTALS)
+    expect(result).toBeFalsy()
+  })
+})
+describe('validate SFI-23 DAX', () => {
+  beforeEach(() => {
+    dax = JSON.parse(JSON.stringify(mockDax1))
+    dax.type = DAX
+  })
+
+  test('returns false if total does not contain payment reference', () => {
+    delete dax.paymentReference
+    const result = validateUpdate(dax, DAX)
+    expect(result).toBeFalsy()
+  })
+  test('returns false if total does not contain payment period', () => {
+    delete dax.paymentPeriod
+    const result = validateUpdate(dax, DAX)
+    expect(result).toBeFalsy()
+  })
+  test('returns false if total does not contain total quarterly payment', () => {
+    delete dax.totalQuarterlyPayment
+    const result = validateUpdate(dax, DAX)
+    expect(result).toBeFalsy()
+  })
+  test('returns false if total does not contain transDate', () => {
+    delete dax.transDate
+    const result = validateUpdate(dax, DAX)
+    expect(result).toBeFalsy()
+  })
+  test('returns false if total does not contain scheme type', () => {
+    delete dax.schemeType
+    const result = validateUpdate(total, TOTALS)
+    expect(result).toBeFalsy()
+  })
+  test('returns falsy if total does not contain calculation date', () => {
+    delete dax.calculationDate
+    const result = validateUpdate(total, TOTALS)
+    expect(result).toBeFalsy()
+  })
+  test('returns falsy if total does not contain invoice number', () => {
+    delete dax.invoiceNumber
     const result = validateUpdate(total, TOTALS)
     expect(result).toBeFalsy()
   })
