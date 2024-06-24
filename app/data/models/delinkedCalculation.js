249d60e0e3f2ee171d4c9d68298c0728ebfdd006
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-06-24 17:56:40 +0100

    npm audit + push new object

---
+++ app/data/models/delinkedCalculation.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, DataTypes) => {
+  const number2 = 2
+  const number15 = 15
+  const delinkedCalculation = sequelize.define('delinkedCalculation', {
+    calculationId: { type: DataTypes.INTEGER, primaryKey: true, allowNull: false },
+    sbi: { type: DataTypes.INTEGER, allowNull: false },
+    frn: { type: DataTypes.BIGINT, allowNull: false },
+    paymentBand1: { type: DataTypes.INTEGER, allowNull: false },
+    paymentBand2: { type: DataTypes.INTEGER, allowNull: false },
+    paymentBand3: { type: DataTypes.INTEGER, allowNull: false },
+    paymentBand4: { type: DataTypes.INTEGER, allowNull: false },
+    percentageReduction1: { type: DataTypes.INTEGER, allowNull: false },
+    percentageReduction2: { type: DataTypes.INTEGER, allowNull: false },
+    percentageReduction3: { type: DataTypes.INTEGER, allowNull: false },
+    percentageReduction4: { type: DataTypes.INTEGER, allowNull: false },
+    progressiveReductions1: { type: DataTypes.DECIMAL(number15, number2), allowNull: false },
+    progressiveReductions2: { type: DataTypes.DECIMAL(number15, number2), allowNull: false },
+    progressiveReductions3: { type: DataTypes.DECIMAL(number15, number2), allowNull: false },
+    progressiveReductions4: { type: DataTypes.DECIMAL(number15, number2), allowNull: false },
+    referenceAmount: { type: DataTypes.DECIMAL(number15, number2), allowNull: false },
+    totalDelinkedPayment: { type: DataTypes.DECIMAL(number15, number2), allowNull: false },
+    paymentAmountCalculated: { type: DataTypes.DECIMAL(number15, number2), allowNull: false }
+  },
+  {
+    tableName: 'delinkedCalculation',
+    freezeTableName: true,
+    timestamps: false
+  })
+  delinkedCalculation.associate = function (models) {
+    delinkedCalculation.hasMany(models.dax, {
+      foreignKey: 'calculationId',
+      as: 'daxEntries'
+    })
+    delinkedCalculation.belongsTo(models.organisation, {
+      foreignKey: 'sbi',
+      as: 'organisations'
+    })
+  }
+  return delinkedCalculation
+}
