62b111bed13824c87def18c0ba9cd68fdb67cf4c
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-06-27 11:20:53 +0100

    updated datatypes

--- app/data/models/delinkedCalculation.js
+++ app/data/models/delinkedCalculation.js
@@ -1,27 +1,30 @@
 module.exports = (sequelize, DataTypes) => {
-  const number2 = 2
-  const number15 = 15
+  const number0 = 0
+  const number11 = 11
+  const number16 = 16
+  const number38 = 38
+
   const delinkedCalculation = sequelize.define('delinkedCalculation', {
-    applicationId: { type: DataTypes.INTEGER, allowNull: false },
-    calculationId: { type: DataTypes.INTEGER, primaryKey: true, allowNull: false },
-    sbi: { type: DataTypes.INTEGER, allowNull: false },
-    frn: { type: DataTypes.INTEGER, allowNull: false },
-    paymentBand1: { type: DataTypes.INTEGER, allowNull: false },
-    paymentBand2: { type: DataTypes.INTEGER, allowNull: false },
-    paymentBand3: { type: DataTypes.INTEGER, allowNull: false },
-    paymentBand4: { type: DataTypes.INTEGER, allowNull: false },
-    percentageReduction1: { type: DataTypes.INTEGER, allowNull: false },
-    percentageReduction2: { type: DataTypes.INTEGER, allowNull: false },
-    percentageReduction3: { type: DataTypes.INTEGER, allowNull: false },
-    percentageReduction4: { type: DataTypes.INTEGER, allowNull: false },
-    progressiveReductions1: { type: DataTypes.DECIMAL(number15, number2), allowNull: false },
-    progressiveReductions2: { type: DataTypes.DECIMAL(number15, number2), allowNull: false },
-    progressiveReductions3: { type: DataTypes.DECIMAL(number15, number2), allowNull: false },
-    progressiveReductions4: { type: DataTypes.DECIMAL(number15, number2), allowNull: false },
-    referenceAmount: { type: DataTypes.DECIMAL(number15, number2), allowNull: false },
-    totalProgressiveReduction: { type: DataTypes.DECIMAL(number15, number2), allowNull: false },
-    totalDelinkedPayment: { type: DataTypes.DECIMAL(number15, number2), allowNull: false },
-    paymentAmountCalculated: { type: DataTypes.DECIMAL(number15, number2), allowNull: false }
+    applicationId: { type: DataTypes.NUMBER(number11, number0), allowNull: false },
+    calculationId: { type: DataTypes.NUMBER(number11, number0), primaryKey: true, allowNull: false },
+    sbi: { type: DataTypes.NUMBER(number38, number0), allowNull: false },
+    frn: { type: DataTypes.STRING(number16), allowNull: false },
+    paymentBand1: { type: DataTypes.STRING, allowNull: false },
+    paymentBand2: { type: DataTypes.STRING, allowNull: false },
+    paymentBand3: { type: DataTypes.STRING, allowNull: false },
+    paymentBand4: { type: DataTypes.STRING, allowNull: false },
+    percentageReduction1: { type: DataTypes.STRING, allowNull: false },
+    percentageReduction2: { type: DataTypes.STRING, allowNull: false },
+    percentageReduction3: { type: DataTypes.STRING, allowNull: false },
+    percentageReduction4: { type: DataTypes.STRING, allowNull: false },
+    progressiveReductions1: { type: DataTypes.STRING, allowNull: false },
+    progressiveReductions2: { type: DataTypes.STRING, allowNull: false },
+    progressiveReductions3: { type: DataTypes.STRING, allowNull: false },
+    progressiveReductions4: { type: DataTypes.STRING, allowNull: false },
+    referenceAmount: { type: DataTypes.STRING, allowNull: false },
+    totalProgressiveReduction: { type: DataTypes.STRING, allowNull: false },
+    totalDelinkedPayment: { type: DataTypes.STRING, allowNull: false },
+    paymentAmountCalculated: { type: DataTypes.STRING, allowNull: false }
   },
   {
     tableName: 'delinkedCalculation',
