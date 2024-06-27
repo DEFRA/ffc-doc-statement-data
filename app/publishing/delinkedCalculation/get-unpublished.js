fa21d3dfb343e6f39578fa54e97491b101584d1c
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2024-06-27 14:47:47 +0100

    removed incorrect schema types based on upated changelog

--- app/publishing/delinkedCalculation/get-unpublished.js
+++ app/publishing/delinkedCalculation/get-unpublished.js
@@ -1,13 +1,8 @@
 const getUnpublishedDelinked = require('./get-unpublished-delinked')
 
 const getUnpublished = async (transaction) => {
-  try {
-    const delinked = await getUnpublishedDelinked(transaction)
-    return delinked
-  } catch (error) {
-    console.error('Error in getUnpublished:', error) // Error logging
-    throw error // Rethrow or handle as needed
-  }
+  const delinked = await getUnpublishedDelinked(transaction)
+  return delinked
 }
 
 module.exports = getUnpublished
--- app/publishing/delinkedCalculation/schema.js
+++ app/publishing/delinkedCalculation/schema.js
@@ -5,7 +5,6 @@ const minSbi = 105000000
 const maxSbi = 999999999
 const minFrn = 1000000000
 const maxFrn = 9999999999
-const number15 = 15
 
 module.exports = Joi.object({
   applicationId: Joi.number().integer().required().messages({
@@ -25,91 +24,57 @@ module.exports = Joi.object({
     'number.max': `sbi should have a maximum value of ${maxSbi}`,
     'any.required': 'The field sbi is not present but it is required'
   }),
-  frn: Joi.number().integer().min(minFrn).max(maxFrn).required().messages({
-    'number.base': 'frn should be a type of number',
-    'number.integer': 'frn should be an integer',
+  frn: Joi.string().min(minFrn).max(maxFrn).required().messages({
     'number.min': `frn should have a minimum value of ${minFrn}`,
     'number.max': `frn should have a maximum value of ${maxFrn}`,
     'any.required': 'The field frn is not present but it is required'
   }),
-  paymentBand1: Joi.number().integer().required().messages({
-    'number.base': 'paymentBand1 should be a type of number',
-    'number.integer': 'paymentBand1 should be an integer',
+  paymentBand1: Joi.string().required().messages({
     'any.required': 'The field paymentBand1 is not present but it is required'
   }),
-  paymentBand2: Joi.number().integer().required().messages({
-    'number.base': 'paymentBand2 should be a type of number',
-    'number.integer': 'paymentBand2 should be an integer',
+  paymentBand2: Joi.string().required().messages({
     'any.required': 'The field paymentBand2 is not present but it is required'
   }),
-  paymentBand3: Joi.number().integer().required().messages({
-    'number.base': 'paymentBand3 should be a type of number',
-    'number.integer': 'paymentBand3 should be an integer',
+  paymentBand3: Joi.string().required().messages({
     'any.required': 'The field paymentBand3 is not present but it is required'
   }),
-  paymentBand4: Joi.number().integer().required().messages({
-    'number.base': 'paymentBand4 should be a type of number',
-    'number.integer': 'paymentBand4 should be an integer',
+  paymentBand4: Joi.string().required().messages({
     'any.required': 'The field paymentBand4 is not present but it is required'
   }),
-  percentageReduction1: Joi.number().integer().required().messages({
-    'number.base': 'percentageReduction1 should be a type of number',
-    'number.integer': 'percentageReduction1 should be an integer',
+  percentageReduction1: Joi.string().required().messages({
     'any.required': 'The field percentageReduction1 is not present but it is required'
   }),
-  percentageReduction2: Joi.number().integer().required().messages({
-    'number.base': 'percentageReduction2 should be a type of number',
-    'number.integer': 'percentageReduction2 should be an integer',
+  percentageReduction2: Joi.string().required().messages({
     'any.required': 'The field percentageReduction2 is not present but it is required'
   }),
-  percentageReduction3: Joi.number().integer().required().messages({
-    'number.base': 'percentageReduction3 should be a type of number',
-    'number.integer': 'percentageReduction3 should be an integer',
+  percentageReduction3: Joi.string().required().messages({
     'any.required': 'The field percentageReduction3 is not present but it is required'
   }),
-  percentageReduction4: Joi.number().integer().required().messages({
-    'number.base': 'percentageReduction4 should be a type of number',
-    'number.integer': 'percentageReduction4 should be an integer',
+  percentageReduction4: Joi.string().required().messages({
     'any.required': 'The field percentageReduction4 is not present but it is required'
   }),
-  progressiveReductions1: Joi.number().precision(number15).required().messages({
-    'number.base': 'progressiveReductions1 should be a type of number',
-    'number.precision': `progressiveReductions1 should have a precision of ${number15}`,
+  progressiveReductions1: Joi.string().required().messages({
     'any.required': 'The field progressiveReductions1 is not present but it is required'
   }),
-  progressiveReductions2: Joi.number().precision(number15).required().messages({
-    'number.base': 'progressiveReductions2 should be a type of number',
-    'number.precision': `progressiveReductions2 should have a precision of ${number15}`,
+  progressiveReductions2: Joi.string().required().messages({
     'any.required': 'The field progressiveReductions2 is not present but it is required'
   }),
-  progressiveReductions3: Joi.number().precision(number15).required().messages({
-    'number.base': 'progressiveReductions3 should be a type of number',
-    'number.precision': `progressiveReductions3 should have a precision of ${number15}`,
+  progressiveReductions3: Joi.string().required().messages({
     'any.required': 'The field progressiveReductions3 is not present but it is required'
   }),
-  progressiveReductions4: Joi.number().precision(number15).required().messages({
-    'number.base': 'progressiveReductions4 should be a type of number',
-    'number.precision': `progressiveReductions4 should have a precision of ${number15}`,
+  progressiveReductions4: Joi.string().required().messages({
     'any.required': 'The field progressiveReductions4 is not present but it is required'
   }),
-  referenceAmount: Joi.number().precision(number15).required().messages({
-    'number.base': 'referenceAmount should be a type of number',
-    'number.precision': `referenceAmount should have a precision of ${number15}`,
+  referenceAmount: Joi.string().required().messages({
     'any.required': 'The field referenceAmount is not present but it is required'
   }),
-  totalProgressiveReduction: Joi.number().precision(number15).required().messages({
-    'number.base': 'totalProgressiveReduction should be a type of number',
-    'number.precision': `totalProgressiveReduction should have a precision of ${number15}`,
+  totalProgressiveReduction: Joi.string().required().messages({
     'any.required': 'The field totalProgressiveReduction is not present but it is required'
   }),
-  totalDelinkedPayment: Joi.number().precision(number15).required().messages({
-    'number.base': 'totalDelinkedPayment should be a type of number',
-    'number.precision': `totalDelinkedPayment should have a precision of ${number15}`,
+  totalDelinkedPayment: Joi.string().required().messages({
     'any.required': 'The field totalDelinkedPayment is not present but it is required'
   }),
-  paymentAmountCalculated: Joi.number().precision(number15).required().messages({
-    'number.base': 'paymentAmountCalculated should be a type of number',
-    'number.precision': `paymentAmountCalculated should have a precision of ${number15}`,
+  paymentAmountCalculated: Joi.string().required().messages({
     'any.required': 'The field paymentAmountCalculated is not present but it is required'
   }),
   type: Joi.string().required().allow(DELINKEDCALCULATION).messages({
