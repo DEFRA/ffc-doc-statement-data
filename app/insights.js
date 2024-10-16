c4c1f102e55a755340e55c1a7347561dc322b23b
Harsh Vasudev <harsh.vasudev.external@eviden.com>
2024-10-16 13:40:29 +0100

    DOC-1505 : Adding temp commit to check issue with service bus

--- app/insights.js
+++ app/insights.js
@@ -4,6 +4,7 @@ function setup () {
   if (process.env.APPLICATIONINSIGHTS_CONNECTION_STRING) {
     appInsights.setup(process.env.APPLICATIONINSIGHTS_CONNECTION_STRING).start()
     console.log('App Insights Running')
+    console.log('App Insights Running... temp log')
     const cloudRoleTag = appInsights.defaultClient.context.keys.cloudRole
     const appName = process.env.APPINSIGHTS_CLOUDROLE
     appInsights.defaultClient.context.tags[cloudRoleTag] = appName
