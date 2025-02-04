6f0654ab8e47060e5ed7bba11761f2ae5ba2b56f
John Barnard <john.barnard.external@eviden.com>
2025-02-04 13:34:55 +0000

    Updated tests to use snapshots

--- test/unit/etl/load-scripts/load-interm-total.test.js
+++ test/unit/etl/load-scripts/load-interm-total.test.js
@@ -15,30 +15,6 @@ describe('loadIntermTotal', () => {
     const startDate = new Date('2023-01-01')
 
     await loadIntermTotal(startDate, mockTransaction)
-
-    const expectedQuery = `
-    INSERT INTO etl_interm_total (
-      payment_ref, quarter, total_amount,
-      transdate
-    )
-    SELECT payment_ref, 
-      D.quarter,
-      SUM(transaction_amount) * -1 as total_amount,
-      transdate 
-    FROM etl_interm_finance_dax D 
-    WHERE D.payment_ref LIKE 'PY%' 
-      AND D.etl_inserted_dt > :startDate
-    GROUP BY transdate, quarter, payment_ref
-    ORDER BY payment_ref;
-  `
-
-    expect(db.sequelize.query).toHaveBeenCalledWith(
-      expectedQuery,
-      {
-        replacements: { startDate },
-        raw: true,
-        transaction: mockTransaction
-      }
-    )
+    expect(db.sequelize.query).toMatchSnapshot()
   })
 })
