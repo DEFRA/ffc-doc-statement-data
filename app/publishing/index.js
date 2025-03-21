30998177dbeea667395e3b759055f1ce96ccf40a
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2025-03-21 15:01:33 +0000

    refactor for streams and batching

--- app/publishing/index.js
+++ app/publishing/index.js
@@ -2,18 +2,25 @@ const { publishingConfig } = require('../config')
 const sendUpdates = require('./send-updates')
 const { ORGANISATION, DELINKED, CALCULATION, TOTAL, DAX, D365 } = require('./types')
 
+const updateTypes = [ORGANISATION, DELINKED, CALCULATION, TOTAL, DAX, D365]
+
+async function * updateGenerator () {
+  for (const type of updateTypes) {
+    yield sendUpdates(type)
+  }
+}
+
 const start = async () => {
   try {
     console.log('Ready to publish data')
-    await sendUpdates(ORGANISATION)
-    await sendUpdates(DELINKED)
-    await sendUpdates(CALCULATION)
-    await sendUpdates(TOTAL)
-    await sendUpdates(DAX)
-    await sendUpdates(D365)
+
+    for await (const promise of updateGenerator()) {
+      await promise
+    }
+
     console.log('All outstanding valid datasets published')
   } catch (err) {
-    console.error(err)
+    console.error('Error during publishing:', err)
   } finally {
     setTimeout(start, publishingConfig.pollingInterval)
   }
--- app/publishing/send-updates.js
+++ app/publishing/send-updates.js
@@ -1,3 +1,5 @@
+const { Readable, Transform } = require('stream')
+const { pipeline } = require('stream/promises')
 const getPrimaryKeyValue = require('./get-primary-key-value')
 const sendMessage = require('./send-message')
 const removeDefunctValues = require('./remove-defunct-values')
@@ -8,19 +10,56 @@ const sendUpdates = async (type) => {
   const updatePublished = require(`./${type}/update-published`)
   let totalPublished = 0
 
-  const outstanding = await getUnpublished()
-  for (const unpublished of outstanding) {
-    const sanitizedUpdate = removeDefunctValues(unpublished)
-    sanitizedUpdate.type = type
-    const isValid = validateUpdate(sanitizedUpdate, type)
-    if (isValid) {
-      await sendMessage(sanitizedUpdate, type)
-      const primaryKey = getPrimaryKeyValue(unpublished, type)
-      await updatePublished(primaryKey)
-      totalPublished++
+  const readStream = new Readable({
+    objectMode: true,
+    async read (size) {
+      try {
+        const batch = await getUnpublished(size)
+        for (const item of batch) {
+          this.push(item)
+        }
+        if (batch.length < size) {
+          this.push(null)
+        }
+      } catch (error) {
+        this.destroy(error)
+      }
     }
+  })
+
+  const processStream = new Transform({
+    objectMode: true,
+    transform (chunk, _encoding, callback) {
+      const processItem = async () => {
+        try {
+          const sanitizedUpdate = removeDefunctValues(chunk)
+          sanitizedUpdate.type = type
+          const isValid = validateUpdate(sanitizedUpdate, type)
+          if (isValid) {
+            await sendMessage(sanitizedUpdate, type)
+            const primaryKey = getPrimaryKeyValue(chunk, type)
+            await updatePublished(primaryKey)
+            totalPublished++
+          }
+          callback()
+        } catch (error) {
+          callback(error)
+        }
+      }
+      processItem()
+    }
+  })
+
+  try {
+    await pipeline(
+      readStream,
+      processStream
+    )
+    console.log('%i %s datasets published', totalPublished, type)
+  } catch (error) {
+    console.error(`Error in sendUpdates for ${type}:`, error)
+    throw error
   }
-  console.log('%i %s datasets published', totalPublished, type)
 }
 
 module.exports = sendUpdates
