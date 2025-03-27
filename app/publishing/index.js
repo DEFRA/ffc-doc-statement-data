31f04106fd3562eb1a15d8f1420377571bf710ee
Leigh Godson <139965284+GodsonLeigh@users.noreply.github.com>
2025-03-27 11:28:37 +0000

    refactored batching, added missing db limitations

--- app/publishing/index.js
+++ app/publishing/index.js
@@ -14,12 +14,10 @@ const processUpdates = async () => {
   }
 }
 
-const start = async () => {
+const start = async (cycleOnce = false) => {
   try {
     console.log('Ready to publish data')
-
     await processUpdates()
-
     console.log('All outstanding valid datasets published')
   } catch (err) {
     console.error('Error during publishing:', err)
--- app/publishing/send-updates.js
+++ app/publishing/send-updates.js
@@ -2,30 +2,37 @@ const getPrimaryKeyValue = require('./get-primary-key-value')
 const sendMessage = require('./send-message')
 const removeDefunctValues = require('./remove-defunct-values')
 const validateUpdate = require('./validate-update')
+const { publishingConfig } = require('../config')
 
 const sendUpdates = async (type) => {
   const getUnpublished = require(`./${type}/get-unpublished`)
   const updatePublished = require(`./${type}/update-published`)
   let totalPublished = 0
-  const batchSize = 250
+  const batchSize = publishingConfig.dataPublishingMaxBatchSizePerDataSource || 250
 
-  const outstanding = await getUnpublished()
+  let outstanding = []
+  do {
+    outstanding = await getUnpublished(null, batchSize)
+    if (outstanding.length) {
+      const batchPromises = outstanding.map(async (record) => {
+        const sanitizedUpdate = removeDefunctValues(record)
+        sanitizedUpdate.type = type
+        const isValid = validateUpdate(sanitizedUpdate, type)
+        if (isValid) {
+          await sendMessage(sanitizedUpdate, type)
+          const primaryKey = getPrimaryKeyValue(record, type)
+          await updatePublished(primaryKey)
+          totalPublished++
+        }
+      })
+      await Promise.all(batchPromises)
+    }
 
-  for (let i = 0; i < outstanding.length; i += batchSize) {
-    const batch = outstanding.slice(i, i + batchSize)
-    const batchPromises = batch.map(async (unpublished) => {
-      const sanitizedUpdate = removeDefunctValues(unpublished)
-      sanitizedUpdate.type = type
-      const isValid = validateUpdate(sanitizedUpdate, type)
-      if (isValid) {
-        await sendMessage(sanitizedUpdate, type)
-        const primaryKey = getPrimaryKeyValue(unpublished, type)
-        await updatePublished(primaryKey)
-        totalPublished++
-      }
-    })
-    await Promise.all(batchPromises)
-  }
+    if (type === 'calculation' && outstanding.length > 0) {
+      break
+    }
+  } while (outstanding.length === batchSize)
+
   console.log('%i %s datasets published', totalPublished, type)
 }
 
