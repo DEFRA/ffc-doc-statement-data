22a6753994a3d66cc407356a4adc6a6c5328fa90
John Barnard <john.barnard.external@eviden.com>
2025-04-22 11:40:37 +0100

    Changed folder for delinked

--- app/config/etl.js
+++ app/config/etl.js
@@ -136,7 +136,7 @@ const config = {
   connectionStr: process.env.AZURE_STORAGE_CONNECTION_STRING,
   storageAccount: process.env.AZURE_STORAGE_ACCOUNT_NAME,
   container: 'etl',
-  dwhExtractsFolder: 'dwh_extracts_sfi23',
+  dwhExtractsFolder: 'dwh_extracts_delinked',
   etlLogsFolder: 'logs',
   etlBatchSize: 2000,
   excludeCalculationData: process.env.EXCLUDE_CALCULATION_DATA,
