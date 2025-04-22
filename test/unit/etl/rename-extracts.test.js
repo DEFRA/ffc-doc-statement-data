605c7aadbd65b21a6469f39fe2c16953a47b9bd2
John Barnard <john.barnard.external@eviden.com>
2025-04-22 15:02:08 +0100

    Updated test

--- test/unit/etl/rename-extracts.test.js
+++ test/unit/etl/rename-extracts.test.js
@@ -69,6 +69,6 @@ test('renameExtracts calls getDWHExtracts and moveFile with correct arguments', 
   await renameExtracts()
 
   expect(getDWHExtracts).toHaveBeenCalled()
-  expect(moveFile).toHaveBeenCalledWith('dwh_extracts_sfi23', appDetailFolder, `${appDetailFolder}/${appDetailFile}`, 'export.csv')
-  expect(moveFile).toHaveBeenCalledWith('dwh_extracts_sfi23', appPaymentFolder, `${appPaymentFolder}/${appPaymentFile}`, 'export.csv')
+  expect(moveFile).toHaveBeenCalledWith('dwh_extracts_delinked', appDetailFolder, `${appDetailFolder}/${appDetailFile}`, 'export.csv')
+  expect(moveFile).toHaveBeenCalledWith('dwh_extracts_delinked', appPaymentFolder, `${appPaymentFolder}/${appPaymentFile}`, 'export.csv')
 })
