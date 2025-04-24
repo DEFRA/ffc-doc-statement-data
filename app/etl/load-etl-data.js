f2e3b5b2d80565d56cd08f8d155c5c3447d7870e
John Barnard <john.barnard.external@eviden.com>
2025-04-24 13:10:36 +0100

    Simple logging to show when etl data loading starts and ends

--- app/etl/load-etl-data.js
+++ app/etl/load-etl-data.js
@@ -36,13 +36,27 @@ const loadETLData = async (startDate) => {
   ]
 
   try {
+    console.log('Start batch 1')
     await Promise.all(promisesBatch1)
+    console.log('End batch 1')
+    console.log('Start batch 2')
     await Promise.all(promisesBatch2)
+    console.log('End batch 2')
+    console.log('Start batch 3')
     await Promise.all(promisesBatch3)
+    console.log('End batch 3')
+    console.log('Start loadIntermPaymentrefOrg')
     await loadIntermPaymentrefOrg(startDate)
+    console.log('End loadIntermPaymentrefOrg')
+    console.log('Start loadTotals')
     await loadTotals(startDate, transaction)
+    console.log('End loadTotals')
+    console.log('Start loadDelinkedCalculation')
     await loadDelinkedCalculation(startDate, transaction)
+    console.log('End loadDelinkedCalculation')
+    console.log('Start loadD365')
     await loadD365(startDate, transaction)
+    console.log('End loadD365')
     await transaction.commit()
     console.log('ETL data successfully loaded')
   } catch (error) {
