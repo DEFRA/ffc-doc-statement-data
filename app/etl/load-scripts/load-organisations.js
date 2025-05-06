41d990f5294884e492f1577a2d8316254a6724a3
John Barnard <john.barnard.external@eviden.com>
2025-05-06 13:49:05 +0100

    Upsert organisations
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/etl/load-scripts/load-organisations.js
+++ app/etl/load-scripts/load-organisations.js
@@ -4,20 +4,40 @@ const { executeQuery } = require('./load-interm-utils')
 
 const loadOrganisations = async (startDate, transaction) => {
   const query = `
-    INSERT INTO ${dbConfig.schema}.organisations (
-      sbi, "addressLine1", "addressLine2",
-      "addressLine3", city, county,
-      postcode, "emailAddress", frn,
-      "name", updated
-    )
+  -- Upsert query for organisations table based on sbi
+  WITH upsert AS (
     SELECT DISTINCT ON (sbi)
       sbi, "addressLine1", "addressLine2",
       "addressLine3", city, county,
-      SUBSTRING(postcode, 1, 7), "emailAddress", frn::integer,
-      "name", NOW()
+      SUBSTRING(postcode, 1, 7) AS postcode, "emailAddress", frn::integer,
+      "name", NOW() AS updated
     FROM ${dbConfig.schema}."etlIntermOrg" O
     WHERE O."etlInsertedDt" > :startDate
-    ORDER BY sbi, "etlInsertedDt" DESC;
+    ORDER BY sbi, "etlInsertedDt" DESC
+  )
+  INSERT INTO ${dbConfig.schema}.organisations (
+    sbi, "addressLine1", "addressLine2",
+    "addressLine3", city, county,
+    postcode, "emailAddress", frn,
+    "name", updated
+  )
+  SELECT
+    upsert.sbi, upsert."addressLine1", upsert."addressLine2",
+    upsert."addressLine3", upsert.city, upsert.county,
+    upsert.postcode, upsert."emailAddress", upsert.frn,
+    upsert."name", upsert.updated
+  FROM upsert
+  ON CONFLICT (sbi) DO UPDATE SET
+    "addressLine1" = EXCLUDED."addressLine1",
+    "addressLine2" = EXCLUDED."addressLine2",
+    "addressLine3" = EXCLUDED."addressLine3",
+    city = EXCLUDED.city,
+    county = EXCLUDED.county,
+    postcode = EXCLUDED.postcode,
+    "emailAddress" = EXCLUDED."emailAddress",
+    frn = EXCLUDED.frn,
+    "name" = EXCLUDED."name",
+    updated = EXCLUDED.updated;
   `
 
   await executeQuery(query, {
