b09d234ad82ccc08cc6b3e127b59782bac12e9b8
John Barnard <john.barnard.external@eviden.com>
2025-05-09 09:45:41 +0100

    more descriptive names
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/data/models/d365.js
+++ app/data/models/d365.js
@@ -2,13 +2,13 @@ const config = require('../../config')
 const dbConfig = config.dbConfig[config.env]
 
 module.exports = (sequelize, DataTypes) => {
-  const paymentReferenceChars = 30
-  const paymentPeriodChars = 200
+  const PAYMENT_REFERENCE_LENGTH = 30
+  const PAYMENT_PERIOD_LENGTH = 200
   const d365 = sequelize.define('d365', {
     d365Id: { type: DataTypes.BIGINT, primaryKey: true, autoIncrement: true, allowNull: false },
-    paymentReference: { type: DataTypes.STRING(paymentReferenceChars), allowNull: false },
+    paymentReference: { type: DataTypes.STRING(PAYMENT_REFERENCE_LENGTH), allowNull: false },
     calculationId: { type: DataTypes.INTEGER },
-    paymentPeriod: { type: DataTypes.STRING(paymentPeriodChars), allowNull: true },
+    paymentPeriod: { type: DataTypes.STRING(PAYMENT_PERIOD_LENGTH), allowNull: true },
     paymentAmount: { type: DataTypes.DECIMAL, allowNull: false },
     transactionDate: { type: DataTypes.DATE, allowNull: false },
     datePublished: { type: DataTypes.DATE, allowNull: true }
--- app/data/models/dax.js
+++ app/data/models/dax.js
@@ -2,13 +2,13 @@ const config = require('../../config')
 const dbConfig = config.dbConfig[config.env]
 
 module.exports = (sequelize, DataTypes) => {
-  const number30 = 30
-  const number200 = 200
+  const PAYMENT_REFERENCE_LENGTH = 30
+  const PAYMENT_PERIOD_LENGTH = 200
   const dax = sequelize.define('dax', {
     daxId: { type: DataTypes.BIGINT, primaryKey: true, autoIncrement: true, allowNull: false },
-    paymentReference: { type: DataTypes.STRING(number30), allowNull: false },
+    paymentReference: { type: DataTypes.STRING(PAYMENT_REFERENCE_LENGTH), allowNull: false },
     calculationId: { type: DataTypes.INTEGER },
-    paymentPeriod: { type: DataTypes.STRING(number200), allowNull: true },
+    paymentPeriod: { type: DataTypes.STRING(PAYMENT_PERIOD_LENGTH), allowNull: true },
     paymentAmount: { type: DataTypes.DECIMAL, allowNull: false },
     transactionDate: { type: DataTypes.DATE, allowNull: false },
     datePublished: { type: DataTypes.DATE, allowNull: true }
