24765033445ef9db0b9fef6ddc34fe80e31228c5
Leigh Godson <leigh.godson@atos.net>
2025-05-27 10:14:45 +0100

    adding marketingYear to d365 data

--- app/data/models/d365.js
+++ app/data/models/d365.js
@@ -4,9 +4,12 @@ const dbConfig = config.dbConfig[config.env]
 module.exports = (sequelize, DataTypes) => {
   const PAYMENT_REFERENCE_LENGTH = 30
   const PAYMENT_PERIOD_LENGTH = 200
+  const marketingYearMinLength = 1000
+  const marketingYearMaxLength = 9999
   const d365 = sequelize.define('d365', {
     d365Id: { type: DataTypes.BIGINT, primaryKey: true, autoIncrement: true, allowNull: false },
     paymentReference: { type: DataTypes.STRING(PAYMENT_REFERENCE_LENGTH), allowNull: false },
+    marketingYear: { type: DataTypes.INTEGER(), allowNull: false, validate: { isInt: true, min: marketingYearMinLength, max: marketingYearMaxLength } },
     calculationId: { type: DataTypes.INTEGER },
     paymentPeriod: { type: DataTypes.STRING(PAYMENT_PERIOD_LENGTH), allowNull: true },
     paymentAmount: { type: DataTypes.DECIMAL, allowNull: false },
