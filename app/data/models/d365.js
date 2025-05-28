195ff671c701517a4735cc241bda98bc5abb1cfd
Leigh Godson <leigh.godson@atos.net>
2025-05-28 09:55:38 +0100

    change min max year length to year range

--- app/data/models/d365.js
+++ app/data/models/d365.js
@@ -4,17 +4,17 @@ const dbConfig = config.dbConfig[config.env]
 module.exports = (sequelize, DataTypes) => {
   const PAYMENT_REFERENCE_LENGTH = 30
   const PAYMENT_PERIOD_LENGTH = 200
-  const marketingYearMinLength = 1000
-  const marketingYearMaxLength = 9999
+  const marketingYearMinYear = 2023
+  const marketingYearMaxYear = 2050
   const d365 = sequelize.define('d365', {
     d365Id: { type: DataTypes.BIGINT, primaryKey: true, autoIncrement: true, allowNull: false },
     paymentReference: { type: DataTypes.STRING(PAYMENT_REFERENCE_LENGTH), allowNull: false },
-    marketingYear: { type: DataTypes.INTEGER(), allowNull: false, validate: { isInt: true, min: marketingYearMinLength, max: marketingYearMaxLength } },
+    marketingYear: { type: DataTypes.INTEGER(), allowNull: false, validate: { isInt: true, min: marketingYearMinYear, max: marketingYearMaxYear } },
     calculationId: { type: DataTypes.INTEGER },
     paymentPeriod: { type: DataTypes.STRING(PAYMENT_PERIOD_LENGTH), allowNull: true },
     paymentAmount: { type: DataTypes.DECIMAL, allowNull: false },
     transactionDate: { type: DataTypes.DATE, allowNull: false },
-    datePublished: { type: DataTypes.DATE, allowNull: true },
+    datePublished: { type: DataTypes.DATE, allowNull: true }
   },
   {
     tableName: 'd365',
