fcf04f1fb03573de75b28ac68b30a40e9ae586e2
John Barnard <john.barnard.external@eviden.com>
2025-07-07 09:29:37 +0100

    Moved the total data model into a constant file
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/data/models/zeroTotals.js
+++ app/data/models/zeroTotals.js
@@ -2,33 +2,14 @@ const config = require('../../config')
 const dbConfig = config.dbConfig[config.env]
 
 module.exports = (sequelize, DataTypes) => {
-  const DECIMAL_PRECISION = 2
-  const DECIMAL_SCALE = 15
-  const INVOICE_NUMBER_LENGTH = 20
-  const SCHEME_TYPE_LENGTH = 50
-  const zeroTotal = sequelize.define('zeroTotal', {
-    calculationId: { type: DataTypes.INTEGER, primaryKey: true, allowNull: false },
-    sbi: { type: DataTypes.INTEGER, allowNull: false },
-    frn: { type: DataTypes.BIGINT, allowNull: false },
-    agreementNumber: { type: DataTypes.INTEGER, allowNull: false },
-    claimId: { type: DataTypes.INTEGER, allowNull: false },
-    schemeType: { type: DataTypes.STRING(SCHEME_TYPE_LENGTH), allowNull: false },
-    calculationDate: { type: DataTypes.DATE, allowNull: false },
-    invoiceNumber: { type: DataTypes.STRING(INVOICE_NUMBER_LENGTH), allowNull: false },
-    agreementStart: { type: DataTypes.DATE, allowNull: false },
-    agreementEnd: { type: DataTypes.DATE, allowNull: false },
-    totalAdditionalPayments: { type: DataTypes.DECIMAL(DECIMAL_SCALE, DECIMAL_PRECISION), allowNull: false },
-    totalActionPayments: { type: DataTypes.DECIMAL(DECIMAL_SCALE, DECIMAL_PRECISION), allowNull: false },
-    totalPayments: { type: DataTypes.DECIMAL(DECIMAL_SCALE, DECIMAL_PRECISION), allowNull: false },
-    updated: { type: DataTypes.DATE },
-    datePublished: { type: DataTypes.DATE, allowNull: true }
-  },
-  {
-    tableName: 'zeroTotals',
-    freezeTableName: true,
-    timestamps: false,
-    schema: dbConfig.schema
-  })
+  const totalDataModel = require('../../constants/total-data-model')(DataTypes)
+  const zeroTotal = sequelize.define('zeroTotal', totalDataModel,
+    {
+      tableName: 'zeroTotals',
+      freezeTableName: true,
+      timestamps: false,
+      schema: dbConfig.schema
+    })
   zeroTotal.associate = function (models) {
     zeroTotal.hasMany(models.dax, {
       foreignKey: 'calculationId',
