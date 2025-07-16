9ea4f038302ac77789665783ec2ce4a4e2896e11
John Barnard <john.barnard.external@eviden.com>
2025-07-16 14:14:17 +0100

    Allow create alerts to accept type but default to ETL type and added zero value alert type to constants
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/messaging/create-alerts.js
+++ app/messaging/create-alerts.js
@@ -3,18 +3,18 @@ const { SOURCE } = require('../constants/source')
 const { ETL_PROCESS_ERROR } = require('../constants/alerts')
 const messageConfig = require('../config/message')
 
-const createAlerts = async (errors) => {
+const createAlerts = async (errors, type = ETL_PROCESS_ERROR) => {
   if (errors?.length) {
-    const alerts = errors.map(createAlert)
+    const alerts = errors.map(error => createAlert(error, type))
     const eventPublisher = new EventPublisher(messageConfig.alertTopic)
     await eventPublisher.publishEvents(alerts)
   }
 }
 
-const createAlert = (error) => {
+const createAlert = (error, type) => {
   return {
     source: SOURCE,
-    type: ETL_PROCESS_ERROR,
+    type,
     data: {
       ...error
     }
