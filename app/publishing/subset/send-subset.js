cc5d568167cbef6918e893efe668515e3e50890e
Sam Plackett <60177449+samplackett@users.noreply.github.com>
2025-08-12 09:49:17 +0100

    populate published for already sent delinkedCalculation, d365. set published in future for subset processes (#120)

--- app/publishing/subset/send-subset.js
+++ app/publishing/subset/send-subset.js
@@ -3,6 +3,9 @@ const validateUpdate = require('../validate-update')
 const sendMessage = require('../send-message')
 const getPrimaryKeyValue = require('../get-primary-key-value')
 const updatePublishedOrganisation = require('../organisation/update-published')
+const updatePublishedDelinkedCalculation = require('../delinkedCalculation/update-published')
+const updatePublishedD365 = require('../d365/update-published')
+const { ORGANISATION, DELINKED_CALCULATION, D365 } = require('../../constants/types')
 
 const sendSubset = async (tablesToTarget) => {
   for (const table of tablesToTarget) {
@@ -14,7 +17,19 @@ const sendSubset = async (tablesToTarget) => {
       if (isValid) {
         await sendMessage(sanitizedUpdate, sanitizedUpdate.type)
         const primaryKey = getPrimaryKeyValue(record, sanitizedUpdate.type)
-        await updatePublishedOrganisation(primaryKey)
+        switch (sanitizedUpdate.type) {
+          case ORGANISATION:
+            await updatePublishedOrganisation(primaryKey)
+            break
+          case DELINKED_CALCULATION:
+            await updatePublishedDelinkedCalculation(primaryKey)
+            break
+          case D365:
+            await updatePublishedD365(primaryKey)
+            break
+          default:
+            throw new Error(`Unknown type: ${sanitizedUpdate.type}`)
+        }
         totalPublished++
       }
     })
--- test/unit/publishing/subset/send-subset.test.js
+++ test/unit/publishing/subset/send-subset.test.js
@@ -3,14 +3,18 @@ const mockValidateUpdate = jest.fn(() => true)
 const mockSendMessage = jest.fn()
 const mockGetPrimaryKeyValue = jest.fn(() => 'primaryKey')
 const mockUpdatePublishedOrganisation = jest.fn()
+const mockUpdatePublishedDelinkedCalculation = jest.fn()
+const mockUpdatePublishedD365 = jest.fn()
 
 jest.mock('../../../../app/publishing/remove-defunct-values', () => mockRemoveDefunctValues)
 jest.mock('../../../../app/publishing/validate-update', () => mockValidateUpdate)
 jest.mock('../../../../app/publishing/send-message', () => mockSendMessage)
 jest.mock('../../../../app/publishing/get-primary-key-value', () => mockGetPrimaryKeyValue)
 jest.mock('../../../../app/publishing/organisation/update-published', () => mockUpdatePublishedOrganisation)
+jest.mock('../../../../app/publishing/delinkedCalculation/update-published', () => mockUpdatePublishedDelinkedCalculation)
+jest.mock('../../../../app/publishing/d365/update-published', () => mockUpdatePublishedD365)
 
-const { ORGANISATION, D365 } = require('../../../../app/constants/types')
+const { ORGANISATION, DELINKED_CALCULATION, D365 } = require('../../../../app/constants/types')
 const sendSubset = require('../../../../app/publishing/subset/send-subset')
 
 describe('sendSubset', () => {
@@ -22,16 +26,15 @@ describe('sendSubset', () => {
     const tablesToTarget = [
       {
         type: ORGANISATION,
-        dataToPublish: [
-          { id: 1, foo: 'bar' },
-          { id: 2, foo: 'baz' }
-        ]
+        dataToPublish: [{ id: 1 }]
+      },
+      {
+        type: DELINKED_CALCULATION,
+        dataToPublish: [{ id: 2 }]
       },
       {
         type: D365,
-        dataToPublish: [
-          { id: 3, foo: 'qux' }
-        ]
+        dataToPublish: [{ id: 3 }]
       }
     ]
     const logSpy = jest.spyOn(console, 'log').mockImplementation(() => { })
@@ -42,32 +45,37 @@ describe('sendSubset', () => {
     expect(mockValidateUpdate).toHaveBeenCalledTimes(3)
     expect(mockSendMessage).toHaveBeenCalledTimes(3)
     expect(mockGetPrimaryKeyValue).toHaveBeenCalledTimes(3)
-    expect(mockUpdatePublishedOrganisation).toHaveBeenCalledTimes(3)
-    expect(logSpy).toHaveBeenCalledWith('%i %s datasets published', 2, ORGANISATION)
+    expect(mockUpdatePublishedOrganisation).toHaveBeenCalledTimes(1)
+    expect(mockUpdatePublishedDelinkedCalculation).toHaveBeenCalledTimes(1)
+    expect(mockUpdatePublishedD365).toHaveBeenCalledTimes(1)
+    expect(logSpy).toHaveBeenCalledWith('%i %s datasets published', 1, ORGANISATION)
+    expect(logSpy).toHaveBeenCalledWith('%i %s datasets published', 1, DELINKED_CALCULATION)
     expect(logSpy).toHaveBeenCalledWith('%i %s datasets published', 1, D365)
     logSpy.mockRestore()
   })
 
+  test('throws error for unknown type', async () => {
+    const tablesToTarget = [{
+      type: 'UNKNOWN',
+      dataToPublish: [{ id: 1 }]
+    }]
+
+    await expect(sendSubset(tablesToTarget)).rejects.toThrow('Unknown type: UNKNOWN')
+  })
+
   test('skips records that are not valid', async () => {
-    mockValidateUpdate.mockReturnValueOnce(false).mockReturnValue(true)
-    const tablesToTarget = [
-      {
-        type: ORGANISATION,
-        dataToPublish: [
-          { id: 1, foo: 'bar' },
-          { id: 2, foo: 'baz' }
-        ]
-      }
-    ]
+    mockValidateUpdate.mockReturnValueOnce(false)
+    const tablesToTarget = [{
+      type: ORGANISATION,
+      dataToPublish: [{ id: 1 }]
+    }]
     await sendSubset(tablesToTarget)
-    expect(mockSendMessage).toHaveBeenCalledTimes(1)
-    expect(mockUpdatePublishedOrganisation).toHaveBeenCalledTimes(1)
+    expect(mockSendMessage).not.toHaveBeenCalled()
+    expect(mockUpdatePublishedOrganisation).not.toHaveBeenCalled()
   })
 
   test('handles empty dataToPublish gracefully', async () => {
-    const tablesToTarget = [
-      { type: ORGANISATION, dataToPublish: [] }
-    ]
+    const tablesToTarget = [{ type: ORGANISATION, dataToPublish: [] }]
     const logSpy = jest.spyOn(console, 'log').mockImplementation(() => { })
     await sendSubset(tablesToTarget)
     expect(mockRemoveDefunctValues).not.toHaveBeenCalled()
