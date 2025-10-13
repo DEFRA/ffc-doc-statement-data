6899525617befa8bc6b544af5aabb7f082686a5a
Sam Plackett <samuel.plackett@eviden.com>
2025-10-13 10:59:58 +0100

    help sonar

--- app/messaging/get-address-lines.js
+++ app/messaging/get-address-lines.js
@@ -1,3 +1,8 @@
+const zero = 0
+const one = 1
+const two = 2
+const three = 3
+
 const isManualEntry = (address) => {
   return address?.address1 || address?.address2 || address?.address3
 }
@@ -11,14 +16,14 @@ const bumpAddressLines = (addressLines) => {
 
   linesAsArray = linesAsArray.filter((line) => line !== null && line !== undefined && line !== '')
 
-  while (linesAsArray.length < 3) {
+  while (linesAsArray.length < three) {
     linesAsArray.push(null)
   }
 
   return {
-    addressLine1: linesAsArray[0],
-    addressLine2: linesAsArray[1],
-    addressLine3: linesAsArray[2]
+    addressLine1: linesAsArray[zero],
+    addressLine2: linesAsArray[one],
+    addressLine3: linesAsArray[two]
   }
 }
 
@@ -30,17 +35,10 @@ const getAddressLines = (address) => {
       addressLine3: address.address3 ?? null
     })
   } else if (isPostcodeLookup(address)) {
-    const {
-      flatName = null,
-      buildingName = null,
-      buildingNumberRange = null,
-      street = null
-    } = address
-
     return bumpAddressLines({
-      addressLine1: flatName,
-      addressLine2: buildingName,
-      addressLine3: buildingNumberRange && street ? `${buildingNumberRange} ${street}` : street || null
+      addressLine1: address.flatName ?? null,
+      addressLine2: address.buildingName ?? null,
+      addressLine3: address.buildingNumberRange && address.street ? `${address.buildingNumberRange} ${address.street}` : address.street || null
     })
   } else {
     return null
