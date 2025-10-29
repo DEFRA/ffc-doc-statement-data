487c2cfda8d6d3f32f313caed7f90ad26b7f386d
Sam Plackett <samuel.plackett@eviden.com>
2025-10-29 14:06:43 +0000

    refactor for sonar

--- app/messaging/format-lookup-address.js
+++ app/messaging/format-lookup-address.js
@@ -4,7 +4,7 @@ const getAddressLine3 = (buildingNumberRange, street) => {
     : null
 }
 
-const formatLookupAddress = (address) => {
+const formatLookupAddress = address => {
   const {
     pafOrganisationName = null,
     flatName = null,
@@ -13,36 +13,22 @@ const formatLookupAddress = (address) => {
     street = null
   } = address
 
+  const addressLine3 = getAddressLine3(buildingNumberRange, street) || null
+
   let addressLine1 = null
   let addressLine2 = null
-  const addressLine3 = getAddressLine3(buildingNumberRange, street) || null
 
   if (pafOrganisationName) {
     addressLine1 = pafOrganisationName
-
-    if (flatName && buildingName) {
-      addressLine2 = `${flatName} ${buildingName}`
-    } else if (flatName) {
-      addressLine2 = flatName
-    } else if (buildingName) {
-      addressLine2 = buildingName
-    } else {
-      addressLine2 = null
-    }
+    addressLine2 = flatName && buildingName
+      ? `${flatName} ${buildingName}`
+      : flatName || buildingName || null
+  } else if (flatName && buildingName) {
+    addressLine1 = flatName
+    addressLine2 = buildingName
   } else {
-    if (flatName && buildingName) {
-      addressLine1 = flatName
-      addressLine2 = buildingName
-    } else if (flatName) {
-      addressLine1 = flatName
-      addressLine2 = null
-    } else if (buildingName) {
-      addressLine1 = buildingName
-      addressLine2 = null
-    } else {
-      addressLine1 = null
-      addressLine2 = null
-    }
+    addressLine1 = flatName || buildingName || null
+    addressLine2 = null
   }
 
   return {
