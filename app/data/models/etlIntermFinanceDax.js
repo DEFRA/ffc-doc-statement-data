3f59fe366bbff2367d66865cbfdb4701bfa10bb2
John Barnard <john.barnard.external@eviden.com>
2025-11-17 16:24:06 +0000

    Added comments to etl interm finance dax fields
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/data/models/etlIntermFinanceDax.js
+++ app/data/models/etlIntermFinanceDax.js
@@ -1,22 +1,24 @@
 const config = require('../../config')
 const dbConfig = config.dbConfig[config.env]
+const comment = "Example Output: Source: DWH | SitiAgri Used on Statement? No, used our ETL service to transform DWH data into our required format"
+const commentDate = "Example Output: 2024-02-09 00:00:00 Source: DWH | SitiAgri Used on Statement? No, used our ETL service to transform DWH data into our required format"
 
 module.exports = (sequelize, DataTypes) => {
   const etlIntermFinanceDax = sequelize.define('etlIntermFinanceDax', {
-    transdate: DataTypes.DATE,
-    invoiceid: DataTypes.STRING,
-    scheme: DataTypes.INTEGER,
-    fund: DataTypes.STRING,
-    marketingyear: DataTypes.INTEGER,
-    month: DataTypes.STRING,
-    quarter: DataTypes.STRING,
-    transactionAmount: DataTypes.DECIMAL,
-    agreementreference: DataTypes.STRING,
-    sitiInvoiceId: DataTypes.INTEGER,
-    claimId: DataTypes.INTEGER,
-    paymentRef: DataTypes.STRING,
-    recid: DataTypes.BIGINT,
-    etlInsertedDt: DataTypes.DATE
+    transdate: { type: DataTypes.DATE, comment: commentDate },
+    invoiceid: { type: DataTypes.STRING, comment },
+    scheme: { type: DataTypes.INTEGER, comment },
+    fund: { type: DataTypes.STRING, comment },
+    marketingyear: { type: DataTypes.INTEGER, comment },
+    month: { type: DataTypes.STRING, comment },
+    quarter: { type: DataTypes.STRING, comment },
+    transactionAmount: { type: DataTypes.DECIMAL, comment },
+    agreementreference: { type: DataTypes.STRING, comment },
+    sitiInvoiceId: { type: DataTypes.INTEGER, comment },
+    claimId: { type: DataTypes.INTEGER, comment },
+    paymentRef: { type: DataTypes.STRING, comment },
+    recid: { type: DataTypes.BIGINT, comment },
+    etlInsertedDt: { type: DataTypes.DATE, comment: commentDate }
   }, {
     tableName: 'etlIntermFinanceDax',
     freezeTableName: true,
