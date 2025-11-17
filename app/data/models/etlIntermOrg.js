22a65098bd4f89878a25f7a54bd2cba2311ea556
John Barnard <john.barnard.external@eviden.com>
2025-11-17 16:37:57 +0000

    Added comments to etl interm org fields
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/data/models/etlIntermOrg.js
+++ app/data/models/etlIntermOrg.js
@@ -1,21 +1,23 @@
 const config = require('../../config')
 const dbConfig = config.dbConfig[config.env]
+const comment = "Example Output: Source: DWH | SitiAgri Used on Statement? No, used our ETL service to transform DWH data into our required format"
+const commentDate = "Example Output: 2024-02-09 00:00:00 Source: DWH | SitiAgri Used on Statement? No, used our ETL service to transform DWH data into our required format"
 
 module.exports = (sequelize, DataTypes) => {
   const etlIntermOrg = sequelize.define('etlIntermOrg', {
     sbi: DataTypes.INTEGER,
-    addressLine1: DataTypes.STRING,
-    addressLine2: DataTypes.STRING,
-    addressLine3: DataTypes.STRING,
-    city: DataTypes.STRING,
-    county: DataTypes.STRING,
-    postcode: DataTypes.STRING,
-    emailAddress: DataTypes.STRING,
-    frn: DataTypes.STRING,
-    name: DataTypes.STRING,
-    partyId: DataTypes.INTEGER,
-    updated: DataTypes.DATE,
-    etlInsertedDt: DataTypes.DATE
+    addressLine1: { type: DataTypes.STRING, comment },
+    addressLine2: { type: DataTypes.STRING, comment },
+    addressLine3: { type: DataTypes.STRING, comment },
+    city: { type: DataTypes.STRING, comment },
+    county: { type: DataTypes.STRING, comment },
+    postcode: { type: DataTypes.STRING, comment },
+    emailAddress: { type: DataTypes.STRING, comment },
+    frn: { type: DataTypes.STRING, comment },
+    name: { type: DataTypes.STRING, comment },
+    partyId: { type: DataTypes.INTEGER, comment },
+    updated: { type: DataTypes.DATE, comment: commentDate },
+    etlInsertedDt: { type: DataTypes.DATE, comment: commentDate }
   }, {
     tableName: 'etlIntermOrg',
     freezeTableName: true,
