6d1bdad20c228dc444d3f5b69ac8bf5ec4c02eef
John Barnard <john.barnard.external@eviden.com>
2025-11-17 16:15:57 +0000

    Added comments to etl interm app contract fields
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/data/models/etlIntermApplicationContract.js
+++ app/data/models/etlIntermApplicationContract.js
@@ -3,12 +3,12 @@ const dbConfig = config.dbConfig[config.env]
 
 module.exports = (sequelize, DataTypes) => {
   const etlIntermApplicationContract = sequelize.define('etlIntermApplicationContract', {
-    contractId: DataTypes.INTEGER,
-    agreementStart: DataTypes.DATE,
-    agreementEnd: DataTypes.DATE,
-    applicationId: DataTypes.INTEGER,
-    pkid: DataTypes.INTEGER,
-    etlInsertedDt: DataTypes.DATE
+    contractId: {type: DataTypes.INTEGER, comment: "Example Output:  2024-02-09 00:00:00 Source: DWH | SitiAgri Used on Statement? No, used our ETL service to transform DWH data into our required format",
+    agreementStart: {type: DataTypes.DATE, comment: "Example Output:  2024-02-09 00:00:00 Source: DWH | SitiAgri Used on Statement? No, used our ETL service to transform DWH data into our required format" },
+    agreementEnd: {type: DataTypes.DATE, comment: "Example Output:  2024-02-09 00:00:00 Source: DWH | SitiAgri Used on Statement? No, used our ETL service to transform DWH data into our required format" },
+    applicationId: {type: DataTypes.INTEGER, comment: "Example Output: Source: DWH | SitiAgri Used on Statement? No, used our ETL service to transform DWH data into our required format"},
+    pkid: {type: DataTypes.INTEGER, comment: "Example Output: Source: DWH | SitiAgri Used on Statement? No, used our ETL service to transform DWH data into our required format"},
+    etlInsertedDt: {type: DataTypes.DATE, comment:"Example Output:  2024-02-09 00:00:00 Source: DWH | SitiAgri Used on Statement? No, used our ETL service to transform DWH data into our required format" }
   }, {
     tableName: 'etlIntermApplicationContract',
     freezeTableName: true,
