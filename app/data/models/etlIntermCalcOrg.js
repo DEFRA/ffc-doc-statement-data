ce13f81b6915e5ae9e54666880fdd0842e9dbbad
John Barnard <john.barnard.external@eviden.com>
2025-11-17 16:21:52 +0000

    Added comments to etl interm app payment fields
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/data/models/etlIntermCalcOrg.js
+++ app/data/models/etlIntermCalcOrg.js
@@ -1,15 +1,17 @@
 const config = require('../../config')
 const dbConfig = config.dbConfig[config.env]
+const comment = "Example Output: Source: DWH | SitiAgri Used on Statement? No, used our ETL service to transform DWH data into our required format"
+const commentDate = "Example Output: 2024-02-09 00:00:00 Source: DWH | SitiAgri Used on Statement? No, used our ETL service to transform DWH data into our required format"
 
 module.exports = (sequelize, DataTypes) => {
   const etlIntermCalcOrg = sequelize.define('etlIntermCalcOrg', {
-    applicationId: DataTypes.INTEGER,
-    calculationId: DataTypes.INTEGER,
-    calculationDate: DataTypes.DATE,
-    sbi: DataTypes.INTEGER,
-    frn: DataTypes.STRING,
-    idClcHeader: DataTypes.INTEGER,
-    etlInsertedDt: DataTypes.DATE
+    applicationId: { type: DataTypes.INTEGER, comment },
+    calculationId:{ type: DataTypes.INTEGER, comment },
+    calculationDate: { type: DataTypes.DATE, comment: commentDate },
+    sbi: { type: DataTypes.INTEGER, comment },
+    frn: { type: DataTypes.STRING, comment },
+    idClcHeader: { type: DataTypes.INTEGER, comment },
+    etlInsertedDt:{ type: DataTypes.DATE, comment: commentDate },
   }, {
     tableName: 'etlIntermCalcOrg',
     freezeTableName: true,
