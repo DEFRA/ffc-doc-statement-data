0aa61a0c0a7b0f2058320f5350548cf158e45134
Oliver Lewington <oliver.lewington@atos.net>
2025-12-02 17:51:19 +0000

    Implementing functionality to remove organisation record if address does not exist

--- app/publishing/organisation/update-published.js
+++ app/publishing/organisation/update-published.js
@@ -1,7 +1,30 @@
 const db = require('../../data')
 
 const updatePublished = async (sbi, transaction) => {
-  await db.organisation.update({ published: new Date() }, { where: { sbi }, transaction })
+  const org = await db.organisation.findOne({
+    where: { sbi },
+    transaction
+  })
+
+  if (!org) {
+    throw new Error(`Organisation with SBI ${sbi} not found`)
+  }
+
+  const noAddress = !org.addressLine1 && !org.addressLine2 && !org.addressLine3 && !org.city && !org.county && !org.postcode
+
+  if (noAddress) {
+    console.log(`Deleting organisation ${sbi} — no address present`)
+    await db.organisation.destroy({
+      where: { sbi },
+      transaction
+    })
+    return
+  }
+
+  await db.organisation.update(
+    { published: new Date() },
+    { where: { sbi }, transaction }
+  )
 }
 
 module.exports = updatePublished
